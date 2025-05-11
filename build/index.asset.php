a3e0cab027e29ae61b220ac82b40d93e86f31069
Matt Jennings <matt@mattjennings.net>
2025-05-11 15:40:58 -0700

    build JS and SCSS

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'b2b1ed8c40b2abc0e970');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '34964d4f5b26a2468807');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '25e53c6f020d4a247b6a');
+<?php return array('dependencies' => array('react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '8d6a0d10a3d364066559');
